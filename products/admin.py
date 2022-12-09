776269da4227217ebdb1c2f46c146c3e3989d3a8
Francis-Mwaniki <francismwaniki630@gmail.com>
2022-12-09 16:02:58 +0300

    webapp-django

---
+++ products/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Products, Offer
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+admin.site.register(Products, ProductsAdmin)
+admin.site.register(Offer, OfferAdmin)
